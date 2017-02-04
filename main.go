d2d67b8c3e851bafcb32fb91b35a06bc2cb04be3
Kara de la Marck <karadelamarck@gmail.com>
2017-02-04 13:55:18 +0000

    kitty

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ import (
 
 func main() {
 
+	kitty := animals.Kitten{}
+	kitty.SetName("Kit Kit")
+	fmt.Println(kitty.GetName())
+
+	kitty2 := &kitty
+	kitty2.SetName("Mr Tom")
+	fmt.Println(kitty.GetName())
+	fmt.Println(kitty2.GetName())
+
 	golden := animals.Dog{}
 	golden.SetName("Beau")
 	fmt.Println(golden.GetName())
