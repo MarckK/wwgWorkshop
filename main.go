f96ed0414789e5663433f3b8c74c5be20b4dbcdf
Kara de la Marck <karadelamarck@gmail.com>
2017-02-04 15:34:50 +0000

    Go object to JSON

--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+
+	"github.com/MarckK/wwg/animals"
 )
 
+var kittens []animals.Kitten
+
 func main() {
-	http.HandleFunc("/hello", HelloWorld)
-	http.HandleFunc("/goodbye", GoodbyeWorld)
+	kittens = []animals.Kitten{
+		animals.Kitten{
+			Name: "Tigg",
+			Hobbies: []string{
+				"Playing with wool",
+				"Eating",
+			},
+		},
+	}
+
+	http.HandleFunc("/list", ListKittens)
+
 	http.ListenAndServe(":9000", http.DefaultServeMux)
+
 }
 
-func HelloWorld(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello")
-}
+func ListKittens(rw http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(kittens)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-func GoodbyeWorld(rw http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	fmt.Fprint(rw, "Goodbye ", name)
+	rw.Write(data)
 }
