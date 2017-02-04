3b0f2040f3b459bb8acb5ebd1640b6d99c021f23
Kara de la Marck <karadelamarck@gmail.com>
2017-02-04 14:21:42 +0000

    average func using range

--- main.go
+++ main.go
@@ -1,24 +1,16 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/MarckK/wwg/animals"
-)
+func average(xs []float64) float64 {
+	total := 0.0
+	for _, v := range xs {
+		total += v
+	}
+	return total / float64(len(xs))
+}
 
 func main() {
-
-	kitty := animals.Kitten{}
-	kitty.SetName("Kit Kit")
-	fmt.Println(kitty.GetName())
-
-	kitty2 := &kitty
-	kitty2.SetName("Mr Tom")
-	fmt.Println(kitty.GetName())
-	fmt.Println(kitty2.GetName())
-
-	golden := animals.Dog{}
-	golden.SetName("Beau")
-	fmt.Println(golden.GetName())
-	fmt.Println(golden.Bark())
+	xs := []float64{98, 93, 77, 82, 83}
+	fmt.Println(average(xs))
 }
