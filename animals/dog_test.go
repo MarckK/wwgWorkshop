1c77f27ec708b991cee48822c1b17a7752d310bf
Kara de la Marck <karadelamarck@gmail.com>
2017-02-13 17:22:52 +0000

    commenting out now redundant kitten_tests and dog_tests

--- animals/dog_test.go
+++ animals/dog_test.go
@@ -1,47 +1,10 @@
 package animals
 
-import (
-	"reflect"
-	"testing"
-)
+import "testing"
 
-func TestSetHobbiesSetsTheHobbies(t *testing.T) {
-	hobbies := []string{"Barking"}
-	dog := Dog{}
-
-	dog.SetHobbies(hobbies)
-
-	if !reflect.DeepEqual(dog.GetHobbies(), hobbies) {
-		t.Fail()
-	}
-}
-
-func TestGetHobbiesGetsTheHobbies(t *testing.T) {
-	hobbies := []string{"Barking"}
-	dog := Dog{Hobbies: hobbies}
-
-	if dog.GetHobbies()[0] != hobbies[0] {
-		t.Fail()
-	}
-}
-
-func TestSetNameSetsTheName(t *testing.T) {
-	dog := Dog{}
-
-	dog.SetName("Spots")
-
-	if dog.Name != "Spots" {
-		t.Fail()
-	}
-}
-
-func TestGetNameGetsTheName(t *testing.T) {
-	dog := Dog{Name: "Spots"}
-
-	if dog.GetName() != "Spots" {
-		t.Fail()
-	}
-}
+//I can comment out almost all the dog testing and my coverage doesn't go down--because pets_tests.go is providing the coverage :)
+//Still: 'PASS coverage: 69.2% of statements'
+//I do need to keep test for Bark() as that functionality is specific to dog and is not being tested in pets_test.go
 
 func TestBarkGetsWoofed(t *testing.T) {
 	dog := Dog{}
@@ -50,3 +13,41 @@ func TestBarkGetsWoofed(t *testing.T) {
 		t.Fail()
 	}
 }
+
+// func TestSetHobbiesSetsTheHobbies(t *testing.T) {
+// 	hobbies := []string{"Barking"}
+// 	dog := Dog{}
+//
+// 	dog.SetHobbies(hobbies)
+//
+// 	if !reflect.DeepEqual(dog.GetHobbies(), hobbies) {
+// 		t.Fail()
+// 	}
+// }
+
+// func TestGetHobbiesGetsTheHobbies(t *testing.T) {
+// 	hobbies := []string{"Barking"}
+// 	dog := Dog{Hobbies: hobbies}
+//
+// 	if dog.GetHobbies()[0] != hobbies[0] {
+// 		t.Fail()
+// 	}
+// }
+
+// func TestSetNameSetsTheName(t *testing.T) {
+// 	dog := Dog{}
+//
+// 	dog.SetName("Spots")
+//
+// 	if dog.Name != "Spots" {
+// 		t.Fail()
+// 	}
+// }
+
+// func TestGetNameGetsTheName(t *testing.T) {
+// 	dog := Dog{Name: "Spots"}
+//
+// 	if dog.GetName() != "Spots" {
+// 		t.Fail()
+// 	}
+// }
--- animals/kitten_test.go
+++ animals/kitten_test.go
@@ -1,44 +1,42 @@
 package animals
 
-import (
-	"reflect"
-	"testing"
-)
+//I can comment out all the kittens testing and my coverage doesn't go down--because pets_tests.go is providing the coverage :)
+//Still: 'PASS coverage: 69.2% of statements'
 
-func TestKSetHobbiesSetsTheHobbies(t *testing.T) {
-	hobbies := []string{"Hunting"}
-	kitten := Kitten{}
+// func TestKSetHobbiesSetsTheHobbies(t *testing.T) {
+// 	hobbies := []string{"Hunting"}
+// 	kitten := Kitten{}
+//
+// 	kitten.SetHobbies(hobbies)
+//
+// 	if !reflect.DeepEqual(kitten.GetHobbies(), hobbies) {
+// 		t.Fail()
+// 	}
+// }
 
-	kitten.SetHobbies(hobbies)
+// func TestKGetHobbiesGetsTheHobbies(t *testing.T) {
+// 	hobbies := []string{"Hunting"}
+// 	kitten := Kitten{Hobbies: hobbies}
+//
+// 	if kitten.GetHobbies()[0] != hobbies[0] {
+// 		t.Fail()
+// 	}
+// }
 
-	if !reflect.DeepEqual(kitten.GetHobbies(), hobbies) {
-		t.Fail()
-	}
-}
+// func TestKSetNameSetsTheName(t *testing.T) {
+// 	kitten := Kitten{}
+//
+// 	kitten.SetName("Pearl")
+//
+// 	if kitten.Name != "Pearl" {
+// 		t.Fail()
+// 	}
+// }
 
-func TestKGetHobbiesGetsTheHobbies(t *testing.T) {
-	hobbies := []string{"Hunting"}
-	kitten := Kitten{Hobbies: hobbies}
-
-	if kitten.GetHobbies()[0] != hobbies[0] {
-		t.Fail()
-	}
-}
-
-func TestKSetNameSetsTheName(t *testing.T) {
-	kitten := Kitten{}
-
-	kitten.SetName("Pearl")
-
-	if kitten.Name != "Pearl" {
-		t.Fail()
-	}
-}
-
-func TestKGetNameGetsTheName(t *testing.T) {
-	kitten := Kitten{Name: "Pearl"}
-
-	if kitten.GetName() != "Pearl" {
-		t.Fail()
-	}
-}
+// func TestKGetNameGetsTheName(t *testing.T) {
+// 	kitten := Kitten{Name: "Pearl"}
+//
+// 	if kitten.GetName() != "Pearl" {
+// 		t.Fail()
+// 	}
+// }
